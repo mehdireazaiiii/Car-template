f0d12ff594a4b363a288bcabc318526e0363d839
mehdi rezaiiii <mehdirezaii09351007807@gmail.com>
2020-06-13 05:10:14 -0700

    v1.0

---
+++ js/loader.js
@@ -0,0 +1,20 @@
+
+function loader(){
+    document.getElementById("gift").style.display="none";
+}
+function topp_1(){
+    document.getElementById("icon_question_1").style.marginBottom="10px";
+    document.getElementById("icon_question_1").style.transition="0.5s";
+}
+function topp_2(){
+    document.getElementById("icon_question_2").style.marginBottom="10px";
+    document.getElementById("icon_question_2").style.transition="0.5s";
+}
+function normal_1(){
+    document.getElementById("icon_question_1").style.marginBottom="0px";
+    document.getElementById("icon_question_1").style.transition="0.5s";
+}
+function normal_2(){
+    document.getElementById("icon_question_2").style.marginBottom="0px";
+    document.getElementById("icon_question_2").style.transition="0.5s";
+}
